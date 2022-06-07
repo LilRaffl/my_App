21fa467d3b666535aaaef0760e7d0e311116ea21
LilRaffl <caninosbrancoscb10@gmail.com>
2022-06-07 17:18:06 -0300

    trabalho com java e xml

--- app/src/main/java/br/com/senac/myapp/LoginActivity.java
+++ app/src/main/java/br/com/senac/myapp/LoginActivity.java
@@ -3,12 +3,60 @@ package br.com.senac.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    //declarar as variaveis global
+    EditText edtEmail, edtSenha;
+    Button btnLogin;
+    TextView txtRecuperaSenha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtRecuperaSenha = findViewById(R.id.txtRecuperaSenha);
+
+        //Cliando o clique do botão
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pegando os valores do email e senha do usuário
+                String email,senha;
+                email = edtEmail.getText().toString();
+                senha = edtSenha.getText().toString();
+                //criando a estrutura de decisão para acesso ao sistema
+                if (true){
+                    //entrar aqui
+
+
+                }
+
+            }
+        });
+
+        txtRecuperaSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+    public void abrirFacebook(View view) {
+        Toast.makeText(getApplicationContext(),
+                "cliquei no botão do Facebfook",
+                Toast.LENGTH_SHORT).show();
+
     }
 }
